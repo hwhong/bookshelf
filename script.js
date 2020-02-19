d0683b882214c57a2d94c470f3c46996ae13f379
warren <hwhong1129@gmail.com>
2020-02-18 23:22:22 -0800

    further

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // client side javascript
 const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+
+const name = prompt("Ehat is your name?");
+appendMessage("You Joined");
+socket.emit("new-user", name);
 
 socket.on("chat-message", data => {
-  console.log(data);
+  appendMessage(data);
 });
+
+messageForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  socket.emit("send-chat-message", message);
+  messageInput.value = "";
+});
+
+function appendMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const io = require("socket.io")(3000);
 
+const users = {};
+
 // each time the user comes on to sever, this will run
 io.on("connection", socket => {
-  socket.emit("chat-message", "hello");
+  socket.on("new-user", name => {
+    users[socket.id] = name;
+  });
+  socket.on("send-chat-message", message => {
+    socket.broadcast.emit("chat-message", message);
+  });
 });
